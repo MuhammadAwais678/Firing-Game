7403a6df7e2a5f93d5f045d962c64d5d3564df38
Muhammad Awais <jawais4210@gmail.com>
2026-04-29 03:02:18 +0500

    Complete

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -17,12 +17,69 @@ public class WeaponSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll > 0) {
+            ScrollNextWepon();
+        }
+        else if (scroll < 0) { 
+            ScrollPreviousWeapon();
+        }
+
+
         for (int i = 0; i < weapons.Length; i++)
         {
             if (Input.GetKeyDown((i + 1).ToString()))
             {
                 currentWeaponIndex = i;
                 ActivateWeapon(currentWeaponIndex);
+            }
+        }
+    }
+
+    void ScrollNextWepon()
+    {
+        int startIndex = currentWeaponIndex + 1;
+
+        for(int i=startIndex; i<weapons.Length; i++)
+        {
+            if (WeaponInventory.instance.IsInventoryWeapon(weapons[i].GetComponent<WeaponController>()))
+            {
+                ActivateWeapon(i);
+                return;
+            }
+        }
+
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if (WeaponInventory.instance.IsInventoryWeapon(weapons[i].GetComponent<WeaponController>()))
+            {
+                ActivateWeapon(i);
+                return;
+            }
+        }
+    }
+
+
+    void ScrollPreviousWeapon()
+    {
+        int previousIndex = currentWeaponIndex - 1;
+
+        for (int i = previousIndex; i >= 0; i--)
+        {
+            if (WeaponInventory.instance.IsInventoryWeapon(weapons[i].GetComponent<WeaponController>()))
+            {
+                ActivateWeapon(i);
+                return;
+            }
+        }
+
+        for (int i = weapons.Length - 1; i >= 0; i--)
+        {
+            if (WeaponInventory.instance.IsInventoryWeapon(weapons[i].GetComponent<WeaponController>()))
+            {
+                ActivateWeapon(i);
+                return;
             }
         }
     }
@@ -34,5 +91,7 @@ public class WeaponSwitcher : MonoBehaviour
           weapons[i].SetActive(i == index);
 
         }
+
+        currentWeaponIndex = index;
     }
 }
